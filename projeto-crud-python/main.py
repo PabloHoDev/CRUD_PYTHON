de55693bbdee77e1254dc9248ffab748046f9ee4
Pablo <pbl.oliveira.dev@gmail.com>
2026-02-15 21:49:09 -0300

    Update main.py

--- projeto-crud-python/main.py
+++ projeto-crud-python/main.py
@@ -1,47 +1,9 @@
-from service.usuario_service import UsuarioService
+from fastapi import FastAPI
+from configuracoes.banco import Base, engine
+from rotas import usuario_rotas
 
+Base.metadata.create_all(bind=engine)
 
-def menu():
-    service = UsuarioService()
+app = FastAPI()
 
-    while True:
-        print("\n--- MENU CRUD ---")
-        print("1 - Criar usuário")
-        print("2 - Listar usuários")
-        print("3 - Atualizar usuário")
-        print("4 - Deletar usuário")
-        print("0 - Sair")
-
-        opcao = input("Escolha: ")
-
-        try:
-            if opcao == "1":
-                nome = input("Nome: ")
-                idade = int(input("Idade: "))
-                service.criar_usuario(nome, idade)
-
-            elif opcao == "2":
-                service.listar_usuarios()
-
-            elif opcao == "3":
-                id_usuario = int(input("ID: "))
-                nome = input("Novo nome: ")
-                idade = int(input("Nova idade: "))
-                service.atualizar_usuario(id_usuario, nome, idade)
-
-            elif opcao == "4":
-                id_usuario = int(input("ID: "))
-                service.deletar_usuario(id_usuario)
-
-            elif opcao == "0":
-                print("👋 Saindo...")
-                break
-
-            else:
-                print("⚠️ Opção inválida!")
-
-        except ValueError:
-            print("❌ Entrada inválida. Use números quando solicitado.")
-
-
-menu()
+app.include_router(usuario_rotas.roteador)
