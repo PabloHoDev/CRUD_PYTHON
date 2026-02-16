06ed64cfb6eb5608392b3fe9a7588312b26ba11e
Pablo <pbl.oliveira.dev@gmail.com>
2026-02-15 21:49:16 -0300

    Delete usuario.py

--- projeto-crud-python/model/usuario.py
+++
@@ -1,8 +0,0 @@
-class Usuario:
-    def __init__(self, id, nome, idade):
-        self.id = id
-        self.nome = nome
-        self.idade = idade
-
-    def __str__(self):
-        return f"ID: {self.id} | Nome: {self.nome} | Idade: {self.idade}"
